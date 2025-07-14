cc1fc24ad5683322a4d382cf5573eedb64bfc8cf
chenyang <chenyang@namibox.com>
2025-07-14 18:02:27 +0800

    。。。

--- app/src/main/java/com/xly/index/SplashActivity.kt
+++ app/src/main/java/com/xly/index/SplashActivity.kt
@@ -3,6 +3,8 @@ package com.xly.index
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.gyf.immersionbar.BarHide
+import com.gyf.immersionbar.ImmersionBar
 import com.xly.R
 
 @SuppressLint("CustomSplashScreen")
@@ -11,6 +13,14 @@ class SplashActivity :AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        initImmersionBar()
+    }
+
+    private fun initImmersionBar() {
+        ImmersionBar.with(this)
+            .statusBarDarkFont(true)
+            .hideBar(BarHide.FLAG_HIDE_NAVIGATION_BAR)
+            .init();
     }
 
 }
