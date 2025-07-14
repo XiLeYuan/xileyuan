e3e06d879d3481d30b12228dae0382bd1c17e8ad
chenyang <chenyang@namibox.com>
2025-07-14 12:28:32 +0800

    。。。

--- app/src/main/java/com/xly/LYMainActivity.kt
+++ app/src/main/java/com/xly/LYMainActivity.kt
@@ -3,4 +3,5 @@ package com.xly
 import android.app.Activity
 
 class LYMainActivity: Activity() {
+
 }
---
+++ app/src/main/java/com/xly/base/LYBaseActivity.kt
@@ -0,0 +1,75 @@
+package com.xly.base
+
+
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.viewbinding.ViewBinding
+
+abstract class LYBaseActivity<VB : ViewBinding, VM : ViewModel> : AppCompatActivity() {
+
+    lateinit var viewBind: VB
+    lateinit var viewModel: VM
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initTabBar()
+        viewBind = inflateBinding(layoutInflater)
+        setContentView(viewBind.root)  // 设置布局
+
+        viewModel = initViewModel()
+        acceptData()
+        initObservers()
+        initView()
+        initOnClick()
+    }
+
+    open fun acceptData() {}
+
+
+    private fun initTabBar() {
+
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+    }
+
+
+    /**初始化viewBind*/
+    abstract fun inflateBinding(layoutInflater: LayoutInflater): VB
+
+    /**初始化viewModel*/
+    abstract fun initViewModel(): VM
+
+
+    open fun initObservers() {}
+
+    open fun initView() {}
+
+    open fun refreshView(o: Any ?= null) {}
+
+    open fun initOnClick() {}
+
+
+    /************************************公共UI提示类***************************************************/
+
+
+
+
+
+
+
+
+}
