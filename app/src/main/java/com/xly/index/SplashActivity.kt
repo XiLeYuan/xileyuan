5cd9fc2bc413a72ecf46d33c9fc47709d30e2d37
chenyang <chenyang@namibox.com>
2025-07-14 17:49:06 +0800

    。。。

--- app/src/main/java/com/xly/index/SplashActivity.kt
+++ app/src/main/java/com/xly/index/SplashActivity.kt
@@ -3,12 +3,14 @@ package com.xly.index
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.xly.R
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity :AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
     }
 
 }
