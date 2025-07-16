9d186ad4e9b9df78289983ac00d8ee86b2542852
chenyang <chenyang@namibox.com>
2025-07-16 23:10:35 +0800

    。。。

--- app/src/main/java/com/xly/business/recommend/view/adapter/CardAdapter.kt
+++ app/src/main/java/com/xly/business/recommend/view/adapter/CardAdapter.kt
@@ -20,6 +20,7 @@ class CardAdapter(private val list: List<Person>) :
     class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val itemLayout: ConstraintLayout = itemView.findViewById(R.id.itemLayout)
         val avatar: LYRoundImageView = itemView.findViewById(R.id.avatar)
+        val topBgImg: ImageView = itemView.findViewById(R.id.topBgImg)
         /*val name: TextView = itemView.findViewById(R.id.name)
         val age: TextView = itemView.findViewById(R.id.age)
         val desc: TextView = itemView.findViewById(R.id.desc)*/
@@ -34,17 +35,22 @@ class CardAdapter(private val list: List<Person>) :
     override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
         val person = list[position]
         holder.avatar.setImageResource(person.avatarRes)
+        if (position == 0 || position == 2 || position == 4) {
+            holder.topBgImg.setImageResource(R.mipmap.stylemax_11)
+        } else {
+            holder.topBgImg.setImageResource(R.mipmap.card_bg)
+        }
         /*holder.name.text = person.name
         holder.age.text = "${person.age}岁"
         holder.desc.text = person.desc*/
 
         // 随机颜色并缓存
-        val color = getRandomColor()
+        /*val color = getRandomColor()
         val width = holder.itemView.width.takeIf { it > 0 } ?: 600
         val height = holder.itemView.height.takeIf { it > 0 } ?: 400
         val bmp = createColorBitmap(color, width, height)
         val blurred = blurBitmap(holder.itemView.context, bmp)
-        holder.itemLayout.background = BitmapDrawable(holder.itemLayout.resources, blurred)
+        holder.itemLayout.background = BitmapDrawable(holder.itemLayout.resources, blurred)*/
 
     }
 
