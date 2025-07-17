14c8fa330266cb54e83db6c404011e7163fa0dbc
chenyang <chenyang@namibox.com>
2025-07-17 13:12:36 +0800

    。。。

--- app/src/main/java/com/xly/business/find/view/MomentImageDetailActivity.kt
+++ app/src/main/java/com/xly/business/find/view/MomentImageDetailActivity.kt
@@ -1,23 +1,24 @@
 package com.xly.business.find.view
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.xly.base.LYBaseActivity
+import com.xly.business.recommend.viewmodel.RecommendViewModel
 import com.xly.middlelibrary.widget.DragPhotoView
+import com.xly.databinding.ActivityImageDetailBinding
 
-class MomentImageDetailActivity : AppCompatActivity() {
+class MomentImageDetailActivity : LYBaseActivity<ActivityImageDetailBinding, RecommendViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val dragPhotoView = DragPhotoView(this)
-        setContentView(dragPhotoView)
 
         val imageResId = intent.getIntExtra("imageResId", 0)
         val momentId = intent.getStringExtra("momentId") ?: ""
         val imageIndex = intent.getIntExtra("imageIndex", 0)
 
         // 正确获取 PhotoView
-        val photoView = dragPhotoView.getPhotoView()
+        val photoView = viewBind.dragPhotoView.getPhotoView()
 
         // 设置转场名称
         photoView.transitionName = "moment_image_${momentId}_$imageIndex"
@@ -26,14 +27,22 @@ class MomentImageDetailActivity : AppCompatActivity() {
         Glide.with(photoView).load(imageResId).into(photoView)
 
         // 设置下拉关闭监听
-        dragPhotoView.setOnDragListener(object : DragPhotoView.OnDragListener {
+        viewBind.dragPhotoView.setOnDragListener(object : DragPhotoView.OnDragListener {
             override fun onDragClose() {
                 supportFinishAfterTransition()
             }
         })
-        dragPhotoView.getPhotoView().setOnClickListener {
+        viewBind.dragPhotoView.getPhotoView().setOnClickListener {
             supportFinishAfterTransition()
         }
+    }
+
+    override fun inflateBinding(layoutInflater: LayoutInflater): ActivityImageDetailBinding {
+        return ActivityImageDetailBinding.inflate(layoutInflater)
+    }
+
+    override fun initViewModel(): RecommendViewModel {
+        return ViewModelProvider(this)[RecommendViewModel::class.java]
     }
 
     private fun finishWithTransition() {
