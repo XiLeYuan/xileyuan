e9041984ba803bc3c4a39215ea65060078a45e97
chenyang <chenyang@namibox.com>
2025-11-03 15:04:00 +0800

    。。。

---
+++ app/src/main/java/com/xly/middlelibrary/widget/LYAnimButton.java
@@ -0,0 +1,13 @@
+package com.xly.middlelibrary.widget;
+
+import android.content.Context;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+
+public class LYAnimButton extends FrameLayout {
+
+    public LYAnimButton(@NonNull Context context) {
+        super(context);
+    }
+}
